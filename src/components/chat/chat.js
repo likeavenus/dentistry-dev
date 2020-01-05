89e18559347be355f7b0d970a95115c7e9047d7b
Rafael <rafael_guliev94@list.ru>
2020-01-06 02:56:53 +0300

    master | работа с блоком чата

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,20 +1,141 @@
+import Typed from 'typed.js';
+import typed from "typed.js/src/typed";
+
 export default function chat() {
     const options = {
         root: null,
-        threshold: 0.5
+        threshold: .8
     };
 
-    const chatElem = document.querySelector('.chat__box');
+    const chatBox = document.querySelector('.chat__box');
 
     const chat = (entries, observer) => {
         entries.forEach(item => {
             if (item.isIntersecting) {
                 console.log(true)
+
+
+                typingAnimation();
+                observer.unobserve(item.target);
             }
         })
     };
 
     const blockObserver = new IntersectionObserver(chat, options);
 
-    blockObserver.observe(chatElem);
+    blockObserver.observe(chatBox);
+
+    const chatArr = [
+        'Здравствуйте, у меня болит зуб, хочу записаться на приём',
+        'Добрый день. В ближайшие даты есть свободные окошки для Вас 🦷',
+        'В какую цену осмотр зуба?',
+        'Осмотр зуба — бесплатно. Обсудим вместе лечение, ответим на все ваши вопросы',
+        'А это больно?',
+        'Наши современные анестезирующие средства заблокируют любые неприятные ощущения',
+        'Супер 😊 Запишите меня, пожалуйста, на прием',
+        'Ждем Вас по адресу: МО, Ленинский р-н, д. Дрожжино, ул. Новое шоссе д. 7 кор. 2, пом. 3'
+    ];
+
+
+
+    const chatTextElems = document.querySelectorAll('.chat__text');
+    const chatBoxElems = document.querySelectorAll('.chat__box-item');
+
+    chatBoxElems.forEach(item => {
+        item.style.display = 'none';
+    });
+
+    function typingAnimation() {
+
+        const speed = 20;
+
+        for (let i = 0; i < chatBoxElems.length; i++) {
+            if (i === 0) {
+                chatBoxElems[i].style.display = 'flex';
+                new Typed(chatTextElems[i], {
+                    strings: [chatArr[i]],
+                    typeSpeed: speed,
+                })
+            } else if (i === 1) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+                }, 4000);
+
+            } else if (i === 2) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+                }, 8000);
+            } else if (i === 3) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+                }, 11000);
+            } else if (i === 4) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+                }, 15000);
+            } else if (i === 5) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+                }, 18000);
+            } else if (i === 6) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+                }, 21000);
+            } else if (i === 6) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+                }, 24000);
+            } else if (i === 7) {
+                setTimeout(function () {
+                    chatBoxElems[i].previousElementSibling.querySelector('.typed-cursor').style.display = 'none';
+                    chatBoxElems[i].style.display = 'flex';
+                    new Typed(chatTextElems[i], {
+                        strings: [chatArr[i]],
+                        typeSpeed: speed,
+                    })
+
+                    chatBoxElems[i].querySelector('.typed-cursor').style.display = 'none';
+                }, 26000);
+            }
+
+        }
+    }
+
+
+
 }
