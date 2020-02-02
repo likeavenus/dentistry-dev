1a2886990d4aeb058fb52e82d1563e1fd66430c5
Rafael <rafael_guliev94@list.ru>
2020-02-02 23:21:53 +0300

    master | стилевые мелкие правки, добавляет шрифты с новым расширением

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,4 +6,17 @@ export default function header() {
         headerElem.classList.toggle('default');
         headerElem.classList.toggle('active');
     });
+
+    const headerLinks = document.querySelectorAll('.header__link');
+
+    if (window.innerWidth < 1260) {
+        headerLinks.forEach(item => {
+            item.addEventListener('click', function () {
+                headerElem.classList.toggle('default');
+                headerElem.classList.toggle('active');
+            });
+        })
+    }
+
+
 }
